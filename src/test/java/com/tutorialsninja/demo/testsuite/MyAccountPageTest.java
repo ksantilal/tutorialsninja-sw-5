52450da2ff990b70a9fb7d52ae75a22a3d41d09b
kapilsantilal <kapilbamania96@gmail.com>
2023-12-12 18:26:30 +0000

    Homework/tutorialsninja-sw-5

---
+++ src/test/java/com/tutorialsninja/demo/testsuite/MyAccountPageTest.java
@@ -0,0 +1,65 @@
+package com.tutorialsninja.demo.testsuite;
+
+import com.tutorialsninja.demo.pages.MyAccountPage;
+import com.tutorialsninja.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyAccountPageTest extends BaseTest {
+
+        MyAccountPage myAccountPage;
+        @BeforeMethod(alwaysRun = true)
+        public void inIt() {
+            myAccountPage = new MyAccountPage();
+        }
+
+        @Test(groups = {"sanity","regression"})
+        public void verifyUserShouldNavigateToRegisterPageSuccessfully(){
+            myAccountPage.clickOnMyAccountLink();
+            myAccountPage.selectMyAccountOptions("Register");
+            Assert.assertEquals(myAccountPage.getRegisterAccountMessage(),"Register Account","Message not displayed");
+        }
+
+        @Test(groups = {"smoke","regression"})
+        public void verifyUserShouldNavigateToLoginPageSuccessfully(){
+            myAccountPage.clickOnMyAccountLink();
+            myAccountPage.selectMyAccountOptions("Login");
+            Assert.assertEquals(myAccountPage.getReturningCustomerMessage(),"Returning Customer","Message not displayed");
+        }
+
+        @Test(groups = {"regression"})
+        public void verifyThatUserRegisterAccountSuccessfully(){
+            myAccountPage.clickOnMyAccountLink();
+            myAccountPage.selectMyAccountOptions("Register");
+            myAccountPage.enterFirstName("Smith");
+            myAccountPage.enterLastName("Dannis");
+            myAccountPage.enterEmailId("smithd123@gmail.com");
+            myAccountPage.enterPhoneNumber("07701217018");
+            myAccountPage.enterPassword("SmithDan");
+            myAccountPage.enterConfirmPassword("SmithDan");
+            myAccountPage.clickSubscribeOnYesRadioButton();
+            myAccountPage.clickOnPrivacyPolicyCheckBox();
+            myAccountPage.clickOnContinue1();
+            Assert.assertEquals(myAccountPage.getAccountCreatedMessage(),"Your Account Has Been Created!","Message not displayed");
+            myAccountPage.clickOnContinueButton2();
+            myAccountPage.clickOnMyAccountTab();
+            myAccountPage.selectMyAccountOptions("Logout");
+            Assert.assertEquals(myAccountPage.getAccountLogoutMessage(),"Account Logout","Message not displayed");
+            myAccountPage.clickOnContinueButton3();
+
+        }
+        @Test(groups = {"regression"})
+        public void verifyThatUserShouldLoginAndLogoutSuccessfully(){
+            myAccountPage.clickOnMyAccountLink();
+            myAccountPage.selectMyAccountOptions("Login");
+            myAccountPage.enterEmailId("smithd123@gmail.com");
+            myAccountPage.enterPassword("SmithDan");
+            myAccountPage.clickOnLogin();
+            // Assert.assertEquals(myAccountPage.getMyAccountText(),"My Account","My account text not displayed");
+            myAccountPage.clickOnMyAccountTab();
+            myAccountPage.selectMyAccountOptions("Logout");
+            Assert.assertEquals(myAccountPage.getAccountLogoutMessage(),"Account Logout","Account Logout text not displayed");
+            myAccountPage.clickOnContinueButton3();
+    }
+}
